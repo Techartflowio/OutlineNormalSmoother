57bf44bc379be34abb8db73e5a09abfe8fc05919
Jason Ma <1312119064@qq.com>
2024-10-25 17:43:01 +0800

    Init 1.0.0

---
+++ Editor/OutlineNormalImporter.cs
@@ -0,0 +1,60 @@
+// Copyright (c) Jason Ma
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.IO;
+using System.Linq;
+using UnityEditor;
+using UnityEngine;
+
+namespace OutlineNormalSmoother
+{
+	public class OutlineNormalImporter : AssetPostprocessor
+    {
+	    public delegate bool OutlineNormalImporterCustomRuleEvent(string assetPath, [MaybeNull] AssetPostprocessor assetPostprocessor);
+        
+	    public static OutlineNormalImporterCustomRuleEvent shouldBakeOutlineNormal = 
+            (assetPath, assetPostprocessor) => assetPath.Contains("_outline.") || assetPath.Contains("_Outline.");
+
+        private void OnPostprocessModel(GameObject go)
+        {
+            if (shouldBakeOutlineNormal(assetPath, this))
+            {
+                OutlineNormalBacker.BakeSmoothedNormalTangentSpaceToMesh(GetSharedMeshesFromGameObject(go));
+            }
+        }
+
+        private static void OnPostprocessAllAssets(string[] importedAssets, string[] deletedAssets, string[] movedAssets, string[] movedFromAssetPaths)
+        {
+            // movedAssets will not call OnPostprocessModel()
+            foreach (var movedAsset in movedAssets)
+            {
+                if (shouldBakeOutlineNormal(movedAsset, null))
+                {
+                    var movedGO = AssetDatabase.LoadAssetAtPath<GameObject>(movedAsset);
+                    OutlineNormalBacker.BakeSmoothedNormalTangentSpaceToMesh(GetSharedMeshesFromGameObject(movedGO));
+                }
+            }
+        }
+
+        internal static List<Mesh> GetSharedMeshesFromGameObject(GameObject go)
+        {
+            List<Mesh> meshes = new ();
+            
+            foreach (var meshFilter in go.GetComponentsInChildren<MeshFilter>())
+            {
+                meshes.Add(meshFilter.sharedMesh);
+            }
+
+            foreach (var skinnedMeshRenderer in go.GetComponentsInChildren<SkinnedMeshRenderer>())
+            {
+                meshes.Add(skinnedMeshRenderer.sharedMesh);
+            }
+
+            return meshes;
+        }
+
+        public static string NormalizeDirectorySeparatorChar(string path) => path.Replace(Path.DirectorySeparatorChar, Path.AltDirectorySeparatorChar);
+    }
+}
